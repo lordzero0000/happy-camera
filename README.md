4d1a7f491627f3b77d25445b93a87dfcfb24e9df
Cloud FullBuster <lordzero0000@gmail.com>
2016-08-12 00:37:42 -0500

    Now the application shows an images according to the emotion

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# happy-camera
+# Happy Camera
+
 Let's show your emotion in pictures!
+
+This project gets a picture from your camera and gets an image based on the emotion that you showed on that picture.
+We use the Emotion API from Microsoft Cognitive Services, so in order to make this work, you should have a subscription key.
+
+## How to run
+
+To run this project you'll need to use the next commands:
+
+```
+npm install
+git clone https://github.com/jhuckaby/webcamjs.git webcamjs
+npm start
+```
+
+And now, you can go to ``http://localhost:1337?token=myAwesomeToken`` (you can set the port using the PORT environment variable) and start to pose to the camera!
+You'll need to use your token from Microsoft Cognitive Services Emotion API, [get it here](https://www.microsoft.com/cognitive-services/en-us/pricing).
--- api.js
+++ api.js
@@ -1,39 +1,69 @@
-function getEmotion(data_uri) {
-  var xhr = new XMLHttpRequest();
-  xhr.withCredentials = false;
+var
+  emotionImages = {
+    anger: "http://i.imgur.com/0U8rb3f.gif",
+    contempt: "http://i.imgur.com/s8VuL5y.jpg",
+    disgust: "http://i.imgur.com/9WPOuHO.gif",
+    fear: "http://i.imgur.com/WFMqLbS.jpg",
+    happiness: "http://i.imgur.com/MddTbq0.gif",
+    neutral: "http://i.imgur.com/Gew0KKT.jpg",
+    sadness: "http://i.imgur.com/TKGN4wj.gif",
+    surprise: "http://i.imgur.com/cuyCIRD.jpg",
+    empty: "http://i.imgur.com/CIubsq7.gif"
+  },
+  getEmotion = (data_uri, token) => {
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = false;
 
-  xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === 4) {
-      console.log(this.responseText);
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        console.log(this.responseText);
+        setEmotionImage(JSON.parse(this.responseText))
+      }
+    });
+
+    xhr.open("POST", "https://api.projectoxford.ai/emotion/v1.0/recognize");
+    xhr.setRequestHeader("content-type", "application/octet-stream");
+    xhr.setRequestHeader("ocp-apim-subscription-key", token);
+
+    xhr.send(getBlob(data_uri));
+  },
+  getBlob = (dataURL) => {
+    var BASE64_MARKER = ';base64,';
+    if (dataURL.indexOf(BASE64_MARKER) == -1) {
+      var parts = dataURL.split(',');
+      var contentType = parts[0].split(':')[1];
+      var raw = decodeURIComponent(parts[1]);
+      return new Blob([raw], { type: contentType });
     }
-  });
+    var parts = dataURL.split(BASE64_MARKER);
+    var contentType = parts[0].split(':')[1];
+    var raw = window.atob(parts[1]);
+    var rawLength = raw.length;
 
-  xhr.open("POST", "https://api.projectoxford.ai/emotion/v1.0/recognize");
-  xhr.setRequestHeader("content-type", "application/json");
-  xhr.setRequestHeader("ocp-apim-subscription-key", "yourEmotionKey");
+    var uInt8Array = new Uint8Array(rawLength);
 
-  xhr.send({ "url": data_uri });
-}
-
-function uploadImage(data_uri) {
-  var xhr = new XMLHttpRequest();
-  xhr.withCredentials = false;
-
-  xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === 4) {
-      console.log(this.responseText);
+    for (var i = 0; i < rawLength; ++i) {
+      uInt8Array[i] = raw.charCodeAt(i);
     }
-  });
 
-  xhr.open("POST", "https://api.imgur.com/3/image");
-  xhr.setRequestHeader("content-type", "application/json");
-  xhr.setRequestHeader("Authorization", "Client-ID yourImgurClientID");
-
-  var image = data_uri.split(',')[1];
-  image = image.replace(/\+/g, '%2B');
-  image = image.replace(/\//g, '%2F');
-  image = image.replace(/\=/g, '%3D');
-  console.log(image);
-
-  xhr.send({ image: image, type: 'base64' });
-}
+    return new Blob([uInt8Array], { type: contentType });
+  },
+  getURLParameter = (name) => {
+    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+  },
+  setEmotionImage = (faces) => {
+    var emotions = faces.map((n) => {
+      var best_emotion = null, best_score = null ;
+      for (var t in n.scores) {
+        if (n.scores.hasOwnProperty(t)) {
+          if (best_score <= n.scores[t]) {
+            best_score = n.scores[t];
+            best_emotion = t;
+          }
+        }
+      }
+      return best_emotion;
+    });
+    var image = (emotions.length != 0) ? emotionImages[emotions[0]] : emotionImages.empty;
+    document.getElementById('my_emotion').innerHTML = '<img src="'+image+'"/>';
+  };
